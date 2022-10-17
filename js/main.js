48110fe9713d5d134579cbabd87c6f4195217907
Obd2004 <obidjonabd1097@gmail.com>
2022-10-17 22:31:00 +0500

    Exam

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var elForm = document.querySelector('.form');
+
+var elInput1 = document.querySelector('.input1');
+var elInput2 = document.querySelector('.input2');
+var elInput3 = document.querySelector('.input3');
+
+var elBtn = document.querySelector('.btn');
+
+var elScore = document.querySelector('.natija');
+
+
+elForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    if(elInput1.value < 60 && elInput2.value < 5.5){
+        elScore.textContent = 'Imtixondan yiqilding'
+    }else if(elInput1.value > 60 && elInput2.value < 5.5 && elInput3.value == 'yoq'){
+        elScore.textContent = 'yiqilding'
+    }else if(elInput1.value < 60 && elInput2.value > 5.5 && elInput3.value == 'yoq'){
+        elScore.textContent = 'yiqilding'
+    }else if(elInput1.value > 60 && elInput2.value < 5.5 && elInput3.value == 'bor'){
+        elScore.textContent = 'Tankdan 500$ olib kel'
+    }else if(elInput1.value < 60 && elInput2.value > 5.5 && elInput3.value == 'bor'){
+        elScore.textContent = 'Tankdan 500$ olib kel'
+    }else{
+        elScore.textContent = 'qandaydir xato yuz berdi'
+    }
+})
